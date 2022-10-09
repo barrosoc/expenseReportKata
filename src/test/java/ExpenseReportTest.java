fc68c8ead726ac16fd26ba3ff3537708d5cde0c5
Javier Lopez Fernandez <javierlopezfernandez@MacBook-Pro-de-Javier.local>
2022-10-09 11:55:49 +0200

    first commit

---
+++ src/test/java/ExpenseReportTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class ExpenseReportTest {
+    @Test
+    public void anyTest() {
+        assertTrue(true);
+    }
+}
